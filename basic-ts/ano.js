778d736a580e87080079139a00d18ee0fe06875c
do-not-do-that <rl7968@naver.com>
2023-08-05 20:56:16 +0900

    feat : 타입 주석과 추론

--- basic-ts/ano.js
+++ basic-ts/ano.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello world!");
+// 타입 주석
+const a = 1;
+const b = "hello ts";
+const c = true;
+// any
+// number
+// string
+// boolean
+// object
+// 타입 추론
+let d = 1;
+// d = "hi"; // 'string' 형식은 'number' 형식에 할당할 수 없습니다.
--- basic-ts/ano.ts
+++ basic-ts/ano.ts
@@ -1,1 +1,13 @@
-console.log("hello world!");
+// 타입 주석
+const a: number = 1;
+const b: string = "hello ts";
+const c: boolean = true;
+// any
+// number
+// string
+// boolean
+// object
+
+// 타입 추론
+let d = 1;
+// d = "hi"; // 'string' 형식은 'number' 형식에 할당할 수 없습니다.
