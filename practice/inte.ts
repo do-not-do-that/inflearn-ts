7cee76fcf86fff78bc0413772ab0996883026d82
Eunbi Hong <ebhong@soft-berry.com>
2023-08-23 12:04:06 +0900

    feat : 데이터 무결성

---
+++ practice/inte.ts
@@ -0,0 +1,22 @@
+interface ICar {
+  name: string;
+  speed: number;
+}
+
+const car = { speed: 0, name: "" };
+
+const carProxy: ICar = new Proxy<ICar>(car, {
+  get(target: ICar, p: keyof ICar) {
+    console.log("get", target, p);
+    return target[p];
+  },
+
+  set(target: ICar, p: keyof ICar, newValue: any) {
+    console.log("set", target, p, newValue);
+    if (p === "speed") target.speed = newValue > 0 ? newValue : 0;
+    return true;
+  },
+});
+
+carProxy.speed = -1;
+console.log(carProxy.speed);
